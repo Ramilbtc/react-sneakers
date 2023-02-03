1057501ae27d4f9ad927222b1b25cbd483bc3515
Ramil <105861531+Ramilbtc@users.noreply.github.com>
2023-02-03 20:49:25 +0300

    11

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,23 @@
-function Card() {
+function Card(props) {
+
+    const onClickButton = () => {
+        alert(props.title)
+    }
+
     return (
         <div className="card">
             <div className="favorite">
                 <img src="/img/heart-unliked.svg" alt="unliked"/>
             </div>
-            <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+            <h5>{props.title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
-                <span>Цена: </span>
-                <b>12999 руб.</b>
+                    <span>Цена: </span>
+                    <b>{props.price}</b>
                 </div>
-                <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
+                <button className="button" onClick={onClickButton}>
+                    <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
                 </button>
             </div>
         </div>
